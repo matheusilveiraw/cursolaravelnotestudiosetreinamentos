110d26570df6995086d87d1cc729a86a1f09d880
Matheus Silveira <matheus.silveiraw@gmail.com>
2024-07-07 21:44:34 -0300

    aula 10

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::any('/any', function() {
 Route::match(['get', 'post'], '/match', function() { 
     return 'permite apenas acessos definidos, o primeiro parametro diz quais são permitidos, nesse caso get e post';
 });
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat = "") { 
+    //ponto de interrgação no categoria para poder deixar ele vazio 
+    //o cat igual a nada para definiri o valor dele como uma string vazia
+    return "o id do produto é: ".$id. "<br>" . "a categoria é: ".$cat;
+});
